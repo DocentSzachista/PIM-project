01990d4fa52c26f76fcbbc3a92b88379bdfe117e
DocentSzachista <idontknow5325@gmail.com>
2022-10-18 11:00:12 +0200

    add example ocr implementation

--- document_ocr/lib/main.dart
+++ document_ocr/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
+import 'package:document_ocr/pages/capture_image.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -26,7 +26,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CaptureImage()
+      // const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
